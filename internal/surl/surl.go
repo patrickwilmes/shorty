ce84dd8dbb18fa26564bf241afcd4d9f462170a2
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2021-01-15 15:50:45 +0100

    check if target url exists

--- internal/surl/surl.go
+++ internal/surl/surl.go
@@ -3,7 +3,13 @@ package surl
 import (
 	"crypto/sha1"
 	"encoding/base64"
+	"errors"
 	"github.com/patrickwilmes/shorty/internal/models"
+	"net/http"
+)
+
+var (
+	errTargetUrlDoesNotExist = errors.New("target url does not exist or is not valid")
 )
 
 type Repository interface {
@@ -22,7 +28,18 @@ func New(surlRepository Repository) Service {
 	return &surlService{SUrlRepository: surlRepository}
 }
 
+func urlExists(targetUrl string) bool {
+	res, err := http.Head(targetUrl)
+	if err != nil {
+		return false
+	}
+	return res.StatusCode == 200
+}
+
 func (su surlService) Create(targeturl string, token models.Token) (string, error) {
+	if !urlExists(targeturl) {
+		return "", errTargetUrlDoesNotExist
+	}
 	h := sha1.New()
 	h.Write([]byte(targeturl))
 	bs := base64.URLEncoding.EncodeToString(h.Sum(nil))
